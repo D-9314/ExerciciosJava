e70dc44792b5153e4f422061648195a8ebd7d22c
Carlos Correia <carlos.correia@sistemafiep.org.br>
2026-09-16 17:23:56 -0300

    Mudei a estrutura e nomes dos programas

--- Ex01Digitacao/src/Ex01_1.java
+++ Ex01Digitacao/src/Ex01_1.java
@@ -14,9 +14,10 @@ public class Ex01_1 {
         // vamos criar algumas variáveis
         String nome = "Juca da Silva";
         int idade = 20;
-        double altura = 1.97;
+        double altura = 1.92;
         char tamanho = 'G';
         // printf imprime variaveis misturadas no texto
+        // dica: altere %f por %.1f e depois %.2f e veja a diferença
         System.out.printf("Olá %s, você tem %d anos e %fm de altura? Você é tamanho %c!\n",
                 nome, idade, altura, tamanho);
     }
